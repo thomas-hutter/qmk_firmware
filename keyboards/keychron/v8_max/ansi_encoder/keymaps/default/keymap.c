15a4b62eaa7c80ab34380894cb57e224e5d645e2
Thomas Hutter <th.hutter@gmail.com>
2026-03-12 19:51:55 +0100

    nav mod

--- keyboards/keychron/v8_max/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/v8_max/ansi_encoder/keymaps/default/keymap.c
@@ -511,7 +511,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
     [NAV] = LAYOUT_ansi_69(        
         _______, _______,  _______,  _______, _______, _______, _______,            DE_F1F,   DE_F2F,   DE_F3F,   DE_F4F,  DE_F5F,  DE_F6F,               KC_BSPC,             KC_ENT,
-        _______, _______,  DUPLINE,  BOLD,    ITAL,    UNDR,                        XXXXXXX,  XXXXXXX,  KC_HOME,  KC_UP,   KC_END,  CTXTMNU,    _______,  _______,             _______,
+        _______, _______,  DUPLINE,  BOLD,    ITAL,    UNDR,                        XXXXXXX, C(KC_HOME), KC_HOME, KC_UP,   KC_END,  C(KC_END),    _______,  _______,             _______,
         _______, KC_LWIN,  KC_LCTL,  KC_LSFT, KC_LCTL, PDIFF,                       XXXXXXX,  KC_PGUP,  KC_LEFT,  KC_DOWN, KC_RGHT, KC_DEL,                _______,             _______,
         KC_LSFT,           BKM_LST,  BKM_TOG, KC_ESC,  NDIFF,   _______,    _______,XXXXXXX,  KC_PGDN,  CKC_SWBK,  CKC_SLNE, CKC_SWFD, SWVD,                       _______,
         _______, _______,            _______,          _______, _______,              CKC_LLCK,           CKC_LLCK,           _______,                      _______,  _______,   _______),        
